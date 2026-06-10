49efcf869226fb1692f77fbd2fc97866d5addd90
Himanshu singla <singlah2505@gmail.com>
2026-06-10 13:38:17 +0530

    some changes

--- src/main/java/com/hkcs/employeemanagement/controller/DepartmentController.java
+++ src/main/java/com/hkcs/employeemanagement/controller/DepartmentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/departments")
+@RequestMapping("/api/departments")
 @RequiredArgsConstructor
 public class DepartmentController {
 
--- src/main/java/com/hkcs/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/hkcs/employeemanagement/controller/EmployeeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/api/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
 
